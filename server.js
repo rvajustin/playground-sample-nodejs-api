ca1cda15e22dd247ca97b088894191c2b93506c7
rvajustin <rvajustin82@gmail.com>
2020-10-05 21:27:19 -0400

    feat: setup

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/healthcheck', function (req, res) {
+   res.end("ok")
+})
+
+app.get('/info', function (req, res) {
+   res.end("nodejs")
+})
+
+var server = app.listen(8080, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
